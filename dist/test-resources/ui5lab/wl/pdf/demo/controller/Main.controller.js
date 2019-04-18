6baf9ac9fb9a66ae7ef1db4b9cff00a8623f9258
Coppey Jeremy <jeremy_coppey@hotmail.com>
2019-04-18 08:44:47 +0000

    added download of pdf

--- dist/test-resources/ui5lab/wl/pdf/demo/controller/Main.controller.js
+++ dist/test-resources/ui5lab/wl/pdf/demo/controller/Main.controller.js
@@ -1,32 +1,33 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("be.wl.PDFExample.controller.Main", {
-		onInit: function() {
+		onInit: function () {
 			this.getView().setModel(new JSONModel({
-				pdfsource: ""
+				pdfsource: "",
+				pdfname: "test file name"
 			}));
 		},
-		onFileChange: function(oEvent) {
+		onFileChange: function (oEvent) {
 			var reader = new FileReader(),
 				me = this;
-			reader.onload = function(oEvent){
+			reader.onload = function (oEvent) {
 				me.getView().getModel().setProperty("/pdfsource", oEvent.target.result);
 			};
 			reader.readAsDataURL(oEvent.getParameter("files")[0]);
 		},
-		onOpenPDFViewer:function(oEvent){
-			if (! this._oDialog) {
+		onOpenPDFViewer: function (oEvent) {
+			if (!this._oDialog) {
 				this._oDialog = sap.ui.xmlfragment("be.wl.PDFExample.view.dialog.PDFViewer", this);
 				this.getView().addDependent(this._oDialog);
 			}
 			this._oDialog.open();
 		},
-		onClose:function(oEvent){
-			if(this._oDialog){
+		onClose: function (oEvent) {
+			if (this._oDialog) {
 				this._oDialog.close();
 			}
 		}
